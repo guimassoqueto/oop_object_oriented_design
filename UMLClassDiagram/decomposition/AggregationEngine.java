923adeb0ee5107e9246698df903baf1db698711d
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-12 10:07:17 -0300

    decomposition: aggregation

---
+++ UMLClassDiagram/decomposition/AggregationEngine.java
@@ -0,0 +1,13 @@
+package decomposition;
+
+public class AggregationEngine {
+    private String type;
+    
+    public AggregationEngine(String type) {
+       this.type = type;
+    }
+    
+    public String getType() {
+       return type;
+    }
+}

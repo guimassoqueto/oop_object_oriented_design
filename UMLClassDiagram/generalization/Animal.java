5dc2eb8c73984feaedf6ed708f9a5d67ba75447c
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-13 07:05:09 -0300

    generalization

---
+++ UMLClassDiagram/generalization/Animal.java
@@ -0,0 +1,19 @@
+package generalization;
+
+public class Animal {
+    private String name;
+    private int legs;
+
+    public Animal(String name, int legs) {
+        this.name = name;
+        this.legs = legs;
+    }
+
+    public void walk() {
+        System.out.printf("%s is walking.\n", this.name);
+    }
+
+    public void countlegs() {
+        System.out.printf("%s has %d legs.\n", this.name, this.legs);
+    }
+}
---
+++ UMLClassDiagram/generalization/Dog.java
@@ -0,0 +1,7 @@
+package generalization;
+
+public class Dog extends Animal {
+    public Dog(String name, int legs) {
+        super(name, legs);
+    }
+}
